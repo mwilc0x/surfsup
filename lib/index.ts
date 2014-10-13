6f9c1f7e7411e52c99f01e359df64c7c16242ac4
Mike Wilcox <mwilcox56@gmail.com>
2014-10-12 21:28:07 -0400

    first method working

--- lib/index.ts
+++ lib/index.ts
@@ -1,23 +1,63 @@
 'use strict';
 
+var _key = (process.env.WEATHER_KEY || ""),
+    _baseURL = 'http://api.worldweatheronline.com/free/v1/',
+    rp = require('request-promise');
+
 module.exports = {
 
-
-  getLocalWeather: () => {
-    
+  /**
+  * Get Local Weather
+  */
+  getLocalWeather: (input) => {
+    input.page = 'weather.ashx?';
+    return rp(constructUrl(input));
   },
 
+  /**
+  * Get Ski & Mountain Weather
+  */
   getSkiMountainWeather: () => {
 
   },
 
+  /**
+  * Get Marine Weather
+  */
   getMarineWeather: () => {
 
   },
 
+  /**
+  * Get Time Zone Data
+  */
   getTimeZoneData: () => {
-
 
   }
 
+}
+
+/**
+* Helper to construct URL with request params
+*/
+function constructUrl(inputs: any): any {
+
+  var base = _baseURL,
+      count = 0;
+
+  base += inputs.page;
+
+  for(var key in inputs) {
+
+    if(count === 0) {
+      base += [key.toString(), '=', inputs[key]].join('');
+    } else {
+      base += ['&', key.toString(), '=', inputs[key]].join('');
+    }
+    count++;
+  }
+
+  base += ['&key=', _key].join('');
+
+  return base;
 }
